05bdf54f48c0fb8611dc8b0fbe63f56a46a6fda5
Liz Healy <lizhealy@Lizs-MacBook-Pro.local>
2017-03-09 10:26:22 -0500

    Initial Commit

--- Swipes and Shakes/ViewController.swift
+++ Swipes and Shakes/ViewController.swift
@@ -9,10 +9,114 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel(frame: CGRect(x: self.view.bounds.width / 2 - 100, y: self.view.bounds.height / 2 - 50, width: 200, height: 100))
+        
+        label.text = "Drag me"
+        
+        label.textAlignment = NSTextAlignment.center
+        
+        view.addSubview(label)
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.wasDragged))
+        
+        label.isUserInteractionEnabled = true
+        
+        label.addGestureRecognizer(gesture)
+        
+        
+        
+        
+        // Original Demo
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+    }
+    
+    
+    func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
+        
+        let translation = gestureRecognizer.translation(in: view)
+        
+        let label = gestureRecognizer.view!
+        
+        label.center = CGPoint(x: self.view.bounds.width / 2 + translation.x, y: self.view.bounds.height / 2 + translation.y)
+        
+        let xFromCenter = label.center.x - self.view.bounds.width / 2
+        
+        //let yFromCenter = label.center.y - self.view.bounds.height / 2
+        
+        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+        
+        let scale = min(abs(100 / xFromCenter), 1)
+        
+        var stretchAndRotation = rotation.scaledBy(x: scale, y: scale)
+        
+        label.transform = stretchAndRotation
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.ended {
+            
+            if label.center.x < 100 {
+                
+                print("Not Chosen")
+                
+            } else if label.center.x > self.view.bounds.width - 100 {
+                
+                print("Chosen")
+            
+            }
+            
+            rotation = CGAffineTransform(rotationAngle: 0)
+            
+            stretchAndRotation = rotation.scaledBy(x: 1, y: 1)
+            
+            label.transform = stretchAndRotation
+            
+            label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+            
+        }
+        
+        
+    }
+    
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            print("Device was shaken")
+        }
+    }
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.right:
+                print("User swiped right")
+            
+            case UISwipeGestureRecognizerDirection.left:
+                print("User swiped left")
+            
+            default:
+                break
+                
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
